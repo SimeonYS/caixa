d2cd219a03cd2fd6793b6ddc856057d4c76bcbdc
SimeonYS <77445467+SimeonYS@users.noreply.github.com>
2021-04-19 13:56:37 +0300

    Update settings.py

--- caixa/settings.py
+++ caixa/settings.py
@@ -3,7 +3,7 @@ BOT_NAME = 'caixa'
 SPIDER_MODULES = ['caixa.spiders']
 NEWSPIDER_MODULE = 'caixa.spiders'
 FEED_EXPORT_ENCODING = 'utf-8'
-# LOG_LEVEL = 'ERROR'
+LOG_LEVEL = 'ERROR'
 DOWNLOAD_DELAY = 0
 USER_AGENT="Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
 
